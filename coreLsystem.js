0c1d708f918c7f2801c6018de8ba4b7f6b5e4281
FiacreR <68591058+FiacreR@users.noreply.github.com>
2021-03-30 20:51:03 +1100

    Added Lindenmayer systems.

---
+++ coreLsystem.js
@@ -0,0 +1,57 @@
+const s = ( p ) => {
+  p.applyRule = function() {
+      p.display();
+      if (iteration>maxIteration) {p.stopIterating()}
+      stepSize *= scalingNormalisationFactor;
+      iteration += 1;
+      sentence = sentence.replace(letterToBeReplaced, function(matched){
+        return rules[matched];
+      });
+      console.log(iteration)
+  }
+
+  p.display = function() {
+      p.colorMode(p.RGB);
+      p.background(20,20,20);
+      p.colorMode(p.HSB, 1);
+      p.stroke(0.5, 0.95, 0.85);
+      p.strokeWeight(2);
+      p.resetMatrix();
+      p.translate(p.width/initialXdivider+p.initialStepSize/2, p.height/initialYdivider+p.initialStepSize/2);
+      for (i = 0; i < sentence.length; i++) {
+          p.letter = sentence[i];
+          switch (p.letter) {
+            case 'F':
+              p.line(0, 0, 0, -stepSize);
+              p.translate(0, -stepSize);
+              break;
+            case 'f':
+              p.translate(0, -stepSize);
+              break;
+            case '+':
+              p.rotate(p.radians(angleIncrement));
+              break;
+            case '-':
+              p.rotate(p.radians(-angleIncrement));
+              break;
+            case '[':
+              p.push();
+              break;
+            case ']':
+              p.pop();
+              break;
+          }
+      }
+  }
+
+  p.stopIterating = function() {
+      clearInterval(interval);
+  }
+
+  p.setup = function() {
+      p.createCanvas(720, 560);
+      p.initialStepSize = stepSize;
+      p.applyRule();
+      interval = setInterval(p.applyRule, 1500);
+  }
+}
